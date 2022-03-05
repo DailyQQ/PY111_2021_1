58e5e5054104369e8c147a70956a56b6d4f9cb51
DailyQQ <Lighting228>
2022-03-05 21:34:00 +0300

    done

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +11,20 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
+    path_nodes = []  # полностью сгоревшие узлы
+    visited_nodes = {node: False for node in g.nodes}  # посещенные узлы
+    wait_nodes = []  # очередь из горящих узлов ожидающих поджечь своих соседей
+    wait_nodes.append(start_node)  # добавляем в конец очереди узел / конец справа
+    visited_nodes[start_node] = True  # при добавлении узла в очередь он считается посещенным
+    while wait_nodes:  # пока есть горящие узлы (очередь не пустая) будем зажигать :)
+        current_node = wait_nodes.pop()  # забираем горящий узле с начала очереди
+        neighbours = g[current_node]
+
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)
+                visited_nodes[neighbour] = True
+        path_nodes.append(current_node)  # полностью сожгли
+    return path_nodes
     print(g, start_node)
     return list(g.nodes)
